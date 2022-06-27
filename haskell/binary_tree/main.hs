c9ac600499a90e49eabbf13713e1440351a02101
matheusafonsouza <matheusafonsouza@gmail.com>
2022-06-27 11:32:02 -0300

    search value function inside int binary tree

--- haskell/binary_tree/main.hs
+++ haskell/binary_tree/main.hs
@@ -2,20 +2,27 @@ module Main where
 
 
 data BinaryTree a = 
-  Empty | 
-  Node a (BinaryTree a) (BinaryTree a) 
+  Empty 
+  | Node Int (BinaryTree a) (BinaryTree a) 
   deriving Show
 
 treeHeight :: BinaryTree a -> Int
 treeHeight Empty = 0
-treeHeight (Node a left right) = 1 + max (treeHeight left) (treeHeight right)
+treeHeight (Node value left right) = 1 + max (treeHeight left) (treeHeight right)
 
 isBalanced :: BinaryTree a -> Bool
 isBalanced Empty = True
-isBalanced (Node a left right) = (abs ((treeHeight left) - (treeHeight right)) <= 1 && (isBalanced left) && (isBalanced right))
+isBalanced (Node value left right) = (abs ((treeHeight left) - (treeHeight right)) <= 1 && (isBalanced left) && (isBalanced right))
+
+searchValue :: BinaryTree a -> Int -> Bool
+searchValue Empty searched = False
+searchValue (Node value left right) searched =
+  | value == searched = True
+  | otherwise = (searchValue left searched) || (searchValue right searched) || False
 
 
 main :: IO ()
 main = do
   isBalanced Empty
   isBalanced (Node 10 (Node 10 Node 10 Empty) (Node 10 Node 10 Empty))
+  searchValue (Node 10 (Node 20 Node 30 Empty) (Node 35 Node 12 Empty)) 20
